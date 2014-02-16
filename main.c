b63802f661158b2919a68e85f59339e544434e97
LM <ladislav@marek.su>
2014-02-16 17:31:34 +0100

    Move command line arguments handling from main to Cli

--- main.c
+++ main.c
@@ -6,49 +6,47 @@
 #include "vm/Repl.h"
 #include "vm/Thread.h"
 #include "vm/CompiledCode.h"
+#include "vm/Cli.h"
 #include <unistd.h>
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-static void printUsage(void);
+static void bootstrapSmalltalk(char *snapshotFileName, char *bootstrapDir);
 
 
 int main(int argc, char **args)
 {
-	char *bootstrapDir = NULL;
-	char *snapshotFileName = "snapshot";
-	char *fileName = NULL;
-	char *eval = NULL;
-	int result = 0;
-	int arg;
+	CliArgs cliArgs;
+	int result = EXIT_SUCCESS;
 
-	while ((arg = getopt(argc, args, "b:s:f:e:")) != -1) {
-		switch (arg) {
-		case 'b':
-			bootstrapDir = optarg;
-			break;
-		case 's':
-			snapshotFileName = optarg;
-			break;
-		case 'f':
-			fileName = optarg;
-			break;
-		case 'e':
-			eval = optarg;
-			break;
-		case ':':
-			printf("Option -%c requires an operand\n", optopt);
-			printUsage();
-			exit(EXIT_FAILURE);
-		case '?':
-			printf("Unrecognized option: '-%c'\n", optopt);
-			printUsage();
-			exit(EXIT_FAILURE);
-		}
+	parseCliArgs(&cliArgs, argc, args);
+	initHeap();
+	bootstrapSmalltalk(cliArgs.snapshotFileName, cliArgs.bootstrapDir);
+
+	if (cliArgs.error != NULL) {
+		printf(cliArgs.error, cliArgs.operand);
+		printf("\n");
+		result = EXIT_FAILURE;
+	} else if (cliArgs.printHelp) {
+		printCliHelp();
+	} else if (cliArgs.fileName != NULL) {
+		/*result = */parseFileAndInitialize(cliArgs.fileName);
+	} else if (cliArgs.eval != NULL) {
+		result = asCInt(evalCode(cliArgs.eval));
+	} else {
+		runRepl();
 	}
 
-	initHeap();
+	freeHandles();
+	freeHeap();
+	return result;
+}
+
+
+
+static void bootstrapSmalltalk(char *snapshotFileName, char *bootstrapDir)
+{
 	FILE *snapshot;
 	if (bootstrapDir) {
 		snapshot = fopen(snapshotFileName, "w+");
@@ -68,33 +66,4 @@ int main(int argc, char **args)
 		initThread(&CurrentThread);
 	}
 	fclose(snapshot);
-
-	if (fileName == NULL && eval == NULL) {
-		runRepl();
-	} else {
-		if (fileName) {
-			parseFileAndInitialize(fileName);
-		}
-		if (eval) {
-			result = asCInt(evalCode(eval));
-		}
-	}
-
-	//printMethodsUsage();
-
-	freeHandles();
-	freeHeap();
-	return result;
-}
-
-
-static void printUsage(void)
-{
-	printf(
-		"Usage:\t<executable> [-b <kernel dir>] [-s <snapshot file>] [-f <file>] [-e <code>]\n"
-		"\t-b bootstrap from kernel directory\n"
-		"\t-s path to snapshot file\n"
-		"\t-f compile classes and evaluate code within specified file\n"
-		"\t-e evaluate code\n"
-	);
 }
---
+++ vm/Cli.h
@@ -0,0 +1,83 @@
+#ifndef CLI_H
+#define CLI_H
+
+#include <unistd.h>
+
+typedef struct {
+	char *error;
+	char operand;
+	char *bootstrapDir;
+	char *snapshotFileName;
+	char *fileName;
+	char *eval;
+	_Bool printHelp;
+} CliArgs;
+
+
+static void parseCliArgs(CliArgs *cliArgs, int argc, char **args)
+{
+	cliArgs->error = NULL;
+	cliArgs->operand = '0';
+	cliArgs->bootstrapDir = NULL;
+	cliArgs->snapshotFileName = "snapshot";
+	cliArgs->fileName = NULL;
+	cliArgs->eval = NULL;
+	cliArgs->printHelp = 0;
+
+	int arg;
+	opterr = 0;
+	while ((arg = getopt(argc, args, "b:s:f:e:")) != -1) {
+		switch (arg) {
+		case 'e':
+			cliArgs->eval = optarg;
+			break;
+		case 'f':
+			cliArgs->fileName = optarg;
+			break;
+		case 's':
+			cliArgs->snapshotFileName = optarg;
+			break;
+		case 'b':
+			cliArgs->bootstrapDir = optarg;
+			break;
+		case 'h':
+			cliArgs->printHelp = 1;
+			break;
+		case '?':
+			cliArgs->operand = optopt;
+			switch (optopt) {
+			case 'e':
+			case 'f':
+			case 's':
+			case 'b':
+				cliArgs->error = "Option -%c requires an operand";
+				break;
+			default:
+				cliArgs->error = "Unrecognized option: '-%c'";
+				cliArgs->operand = optopt;
+				break;
+			}
+			break;
+		}
+	}
+	opterr = 1;
+
+	if (cliArgs->eval != NULL && cliArgs->fileName != NULL) {
+		cliArgs->error = "Cannot use -e and -f together";
+	}
+}
+
+
+static void printCliHelp(void)
+{
+	printf(
+		"Usage:\t<executable> [-e <code>] [-f <file>] [-s <snapshot file>] [-b <kernel dir>]\n"
+		"\t-e evaluate code\n"
+		"\t-f compile classes and evaluate code within specified file\n"
+		"\t-s path to snapshot file\n"
+		"\t-b bootstrap from kernel directory\n"
+		"\t-h prints this help"
+	);
+}
+
+#endif
